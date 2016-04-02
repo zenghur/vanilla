0dcfab376b526164b4b272787abfe62772733ae9
zenghur <254715579@qq.com>
2016-04-02 18:18:23 +0800

    use pthread_once to implement Singleton.

--- main.cpp
+++ main.cpp
@@ -5,11 +5,25 @@
 //  Created by ZengHongru on 16/4/1.
 //
 //
-
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <pthread.h>
 #include <iostream>
+
+#include "GenericSingleton.h"
+
+using namespace std;
+
+struct T {
+    T() {
+        i = 3;
+    }
+    int i;
+};
 
 int main()
 {
-    
+    cout << vanila::GenericSingleton<T>::getInstance().i << endl;
     return 0;
 }
---
+++ utils/GenericSingleton.h
@@ -0,0 +1,83 @@
+//
+//  GenericSingleton.hpp
+//  Vanilla
+//
+//  Created by ZengHongru on 16/4/2.
+//
+//
+
+// Double-checked locking pattern(DCLP)的做法有问题，stackoverflow有讨论。
+// Scott Meyers and Andrei Alexandrescu对此有话要说！文章名叫C++ and the Perils of Double-Checked Locking。
+
+#ifndef GenericSingleton_hpp
+#define GenericSingleton_hpp
+
+#include "Noncopyable.h"
+
+#include <pthread.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <cstdlib>
+
+namespace vanila {
+    
+template <typename T>
+class GenericSingleton : private vanilla::Noncopyable {
+public:
+    static T &getInstance();
+private:
+    GenericSingleton();
+    ~GenericSingleton();
+    static void init();
+    static void destroy();
+    static pthread_once_t control_;
+    static T *instance_;
+};
+  
+template <typename T>
+T* GenericSingleton<T>::instance_ = NULL;
+    
+template <typename T>
+pthread_once_t GenericSingleton<T>::control_ = PTHREAD_ONCE_INIT;
+    
+template <typename T>
+T &GenericSingleton<T>::getInstance()
+{
+    pthread_once(&control_, &GenericSingleton<T>::init);
+    return *instance_;
+}
+
+template <typename T>
+void GenericSingleton<T>::init()
+{
+    instance_ = new T();
+    atexit(&GenericSingleton<T>::destroy);
+}
+    
+template <typename T>
+GenericSingleton<T>::GenericSingleton()
+{
+    
+}
+
+template <typename T>
+GenericSingleton<T>::~GenericSingleton()
+{
+    
+}
+    
+template <typename T>
+void GenericSingleton<T>::destroy()
+{
+    if (instance_) {
+        delete instance_;
+        instance_ = NULL;
+    }
+}
+
+}
+
+
+
+#endif /* GenericSingleton_hpp */
