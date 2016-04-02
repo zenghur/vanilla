bac3d5c275ea0ed9f62ac469ffc6fddbb31e3cc7
zenghur <zenghongru@gmail.com>
2016-04-02 16:44:45 +0800

    alas

--- main.cpp
+++ main.cpp
@@ -7,8 +7,22 @@
 //
 
 #include <iostream>
+#include "Singleton.h"
+
+using namespace std;
+
+class T {
+public:
+    T()
+    {
+        i = 4;
+    }
+public:
+    int i;
+};
 
 int main()
 {
+    cout << Singleton<T>::getInstance().i << endl;
     return 0;
 }
