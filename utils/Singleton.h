39b99bceb4c89da89397a1028129487140abe2e4
zenghur <zenghongru@gmail.com>
2016-04-02 16:38:31 +0800

    Singleton.h

--- utils/Singleton.h
+++ utils/Singleton.h
@@ -4,10 +4,41 @@
 //
 //  Created by ZengHongru on 16/4/1.
 //
-//
+//  In C++11, this is thread-safe method.
 
 #ifndef Singleton_h
 #define Singleton_h
 
+#include "Noncopyable.h"
+
+template <typename T>
+class Singleton : private vanilla::Noncopyable {
+public:
+    static T &getInstance();
+private:
+    Singleton();
+    ~Singleton();
+};
+
+template <typename T>
+T &Singleton<T>::getInstance()
+{
+    static T instance;
+    return instance;
+}
+
+template <typename T>
+Singleton<T>::Singleton()
+{
+    
+}
+
+template <typename T>
+Singleton<T>::~Singleton()
+{
+    
+}
+
 
 #endif /* Singleton_h */
+
